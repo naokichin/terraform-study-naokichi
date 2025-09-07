e221e4ba5c7e6006f003f784be1823e48be1ff87
naokichi <naoki_5158@yahoo.co.jp>
2025-09-08 08:56:49 +0900

    Initial commit: terraform-study-naokichi

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1" # 東京リージョン
+}
+
+# VPC を作成
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
