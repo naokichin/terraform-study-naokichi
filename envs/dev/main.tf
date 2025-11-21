933fb2ed7d552bbdf20af94a96b3b81f731b4b12
naokichi <naoki_5158@yahoo.co.jp>
2025-11-22 00:57:36 +0900

    fix: use data source to fetch latest amazon linux ami

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+# ← 最新の Amazon Linux 2023 を常に取得
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
 module "network" {
   source = "../../modules/network"
 
@@ -9,7 +20,7 @@ module "network" {
   vpc_cidr            = "10.0.0.0/16"
   private_subnet_cidr = "10.0.1.0/24"
 
-  ami_id = "ami-060e6df00d4e4c5c2"  # ← 東京リージョン用AMIを指定（後で教える）
+  ami_id        = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
 }
 
